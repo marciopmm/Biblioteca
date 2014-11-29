107e6e2d76a2f20164e9e2ae2f96d8c28c3a6a39
Marcio Martins <marciopmm@gmail.com>
2014-11-29 11:50:11 -0200

    Criação da classe Associado

---
+++ Biblioteca/Classes/Associado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Classes
+{
+    class Associado
+    {
+        // Propriedades
+        // Nome, endereço, telefone, celular
+        public string Nome;
+        public string Cpf;
+        public string Rg;
+        public string Endereco;
+        public string Complemento;
+        public string Bairro;
+        public string Cidade;
+        public string Cep;
+        public char Sexo;
+        public string TelefoneResidencial;
+        public string TelefoneCelular;
+        public string TelefoneComercial;
+        public DateTime DataNascimento;
+
+        // Métodos
+        public DateTime Emprestar(Livro livro)
+        {
+
+        }
+
+        public void Devolver(Livro livro)
+        {
+
+        }
+    }
+}
